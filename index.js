65511bf7db296c2ac84db9f1d2bbe74098be9e62
Medrine Musabi Mulindi <medrine.mulindi@student.moringaschool.com>
2023-07-24 23:12:17 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Add your code here
+function submitData(name, email) {
+  const userData = { name: name, email: email };
+
+  const configurationObject = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify(userData)
+  };
+
+  return fetch('http://localhost:3000/users', configurationObject)
+    .then(response => response.json())
+    .then(data => {
+      // Assuming the response object contains a property called 'id'
+      const newId = data.id;
+      const idElement = document.createElement('p');
+      idElement.textContent = newId;
+      document.body.appendChild(idElement);
+    })
+    .catch(error => {
+      const errorMessageElement = document.createElement('p');
+      errorMessageElement.textContent = error.message;
+      document.body.appendChild(errorMessageElement);
+    });
+}
+
+
+submitData('Steve', 'steve@steve.com');
